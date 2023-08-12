8658ad904c8c909c08b637a840616b6ba0d98871
wl00795897 <wl00795897@gmail.com>
2023-08-12 19:58:21 +0800

    Add Footer w/o RWD

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,166 @@
-
-
-const Home = () =>{
-    return(
-        <div>page            
-        </div>
-    )
-}
-export default Home
+const Home = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,
+      cupiditate? Fugiat quibusdam maxime accusantium sapiente nisi similique
+      voluptate consectetur modi quis alias tempora voluptas minima magnam nam
+      magni, pariatur nesciunt ipsam sed a eius rem asperiores non totam!
+      Inventore nesciunt recusandae cum facilis ad quidem, atque eveniet autem
+      itaque et at natus voluptates tenetur nisi vero eos placeat cupiditate
+      numquam, excepturi ab dolor quisquam, molestias dolorem. Molestias,
+      quibusdam voluptatem exercitationem nobis voluptate perferendis asperiores
+      ipsam quisquam non reiciendis facilis labore inventore dignissimos dolore,
+      fugiat laudantium dolorum commodi quos ducimus excepturi nihil laboriosam
+      molestiae impedit distinctio. Nesciunt incidunt unde nobis maiores labore
+      reiciendis consequatur magni ratione, officia perspiciatis cumque,
+      adipisci recusandae quibusdam error dolore eligendi eaque! Id minus autem
+      laboriosam commodi reprehenderit aspernatur obcaecati saepe earum? Ducimus
+      recusandae expedita doloremque molestiae eius tempore velit minima
+      corrupti soluta aut temporibus ratione fugit, assumenda facere corporis
+      vitae dolorum nam itaque amet quidem consequuntur rerum accusamus dolore?
+      Labore, pariatur corrupti! Vero, ex itaque. Sunt, similique natus
+      doloribus explicabo eveniet animi quidem inventore sit cumque magni odio
+      obcaecati aperiam enim, voluptatibus reprehenderit iure voluptatum minima
+      vel tempora ea totam illum expedita? Perspiciatis repellat obcaecati,
+      provident ipsam consequatur tempora quas nam nulla! Ad adipisci doloribus
+      et fugit atque illum hic, inventore ipsa ratione esse, minima voluptates
+      itaque? Iure aliquam illo facere temporibus officiis voluptatum ipsam
+      molestias quam quod minus? Nostrum, ipsa eligendi quos autem hic illo fuga
+      eum eaque delectus amet dolor sit atque qui eveniet fugit necessitatibus
+      facilis, aliquid tempore dolores odio temporibus in itaque! Obcaecati quo
+      incidunt veritatis nobis eius earum accusamus fuga autem odio unde!
+      Explicabo enim doloribus consequatur! Quam minus dolor blanditiis optio,
+      obcaecati explicabo nemo quisquam voluptas accusamus reprehenderit ratione
+      totam quae! Deserunt, dolorem facilis quibusdam assumenda harum inventore
+      minus! Ipsa neque perspiciatis dicta iure eveniet voluptates labore
+      repellat ipsam eum ducimus sint animi quibusdam cupiditate consectetur
+      nemo, tenetur ratione id illo rem architecto nobis voluptatibus ab quos?
+      Neque asperiores, amet earum, quod voluptas dolores assumenda vitae
+      dolorum, ab esse at maxime nostrum itaque et? Illo minima dolores
+      temporibus laudantium vero doloremque consectetur facilis error dolor
+      exercitationem, repudiandae ab iste ullam excepturi, minus sit hic modi
+      dolore at! Tempora nostrum provident dolores consequuntur, eius voluptate
+      in deserunt sint quia ratione veniam magnam ut soluta porro. Itaque
+      ratione voluptates vitae. Consectetur assumenda voluptas exercitationem
+      accusamus beatae pariatur, sequi quibusdam in! Incidunt saepe rem, omnis
+      dolorum esse earum iste totam nesciunt dolorem eaque. Cumque magnam
+      repudiandae sequi illum veniam? Nemo id veniam, maiores at molestiae earum
+      iusto repellat harum quod perspiciatis expedita rem atque? Quam neque
+      animi excepturi, quaerat veritatis iste, ipsum explicabo hic vitae
+      architecto illo magni recusandae ea omnis fuga, alias enim temporibus
+      exercitationem voluptatem quidem eligendi sequi esse. Velit saepe fugiat
+      doloribus illo autem nobis. Earum aperiam esse ullam sit reiciendis nam
+      nisi sapiente nulla, mollitia maiores eveniet commodi temporibus
+      recusandae veniam repellendus libero itaque magnam eligendi quis pariatur?
+      Minus in consequuntur assumenda, voluptates optio delectus doloribus,
+      dolore deserunt debitis voluptatem quis pariatur saepe voluptate
+      aspernatur eligendi, atque molestias obcaecati a earum repudiandae eaque
+      quibusdam iure. Praesentium aperiam harum fugit nemo assumenda velit
+      maiores porro adipisci eius in magnam ad, facilis eum facere iusto! Omnis
+      dolorum voluptatem a quidem, magni temporibus, numquam tenetur officiis
+      cupiditate quasi totam quibusdam molestias incidunt similique, repellat
+      aspernatur nemo laborum! Maxime repellat odit deleniti eos libero,
+      repellendus esse voluptatibus facilis praesentium nostrum quas. Rem,
+      reiciendis? Minima voluptates dolorum necessitatibus error obcaecati,
+      beatae, facilis assumenda aspernatur itaque autem laboriosam consequatur
+      corrupti ipsum soluta, fugit placeat rem totam? Fuga perspiciatis et vel
+      harum odit quaerat quos quod magni nam, at nobis optio delectus maiores
+      qui, quae quisquam maxime, tempore tenetur ad ducimus dolor omnis
+      exercitationem nisi? Alias sint aperiam nulla quis maxime eius odio
+      officia blanditiis iure? Itaque maiores fugit eum quaerat ipsum, voluptas
+      saepe quia nihil asperiores dicta laborum illum eligendi architecto
+      aliquam ea unde dignissimos harum nam. Atque minus quam consequuntur nisi
+      aut omnis molestiae, eligendi amet nemo cum ipsam quibusdam in
+      voluptatibus quia, soluta exercitationem repellendus. Officia, vero
+      distinctio. Dignissimos saepe quia, dolore optio ex, laboriosam aut
+      maiores, itaque reiciendis eligendi similique iure ducimus praesentium
+      facilis eius explicabo vitae sint nihil! Expedita neque quasi, itaque
+      maxime soluta officiis error vero exercitationem optio rem provident
+      eligendi at accusamus cumque deleniti distinctio similique aliquam
+      pariatur est eius dolore! Facere animi modi eius tenetur quis, dicta
+      adipisci repellendus maiores necessitatibus molestiae. Rem quas earum
+      dolore sunt, nam inventore aperiam maxime eveniet officia porro? Dolorem
+      reiciendis libero quasi impedit modi velit iure voluptas eum beatae alias
+      adipisci, quaerat omnis repudiandae error laudantium deleniti rerum illo
+      aliquam possimus maiores quae harum. Animi deserunt eos natus dolorem
+      laborum, omnis debitis mollitia deleniti dolorum maiores et tenetur
+      laboriosam eius rerum ex, doloribus sed! Quia reiciendis id aliquam culpa
+      maxime quos? Quos molestias laboriosam aut iusto exercitationem dolore
+      optio quis nobis, suscipit ducimus quae debitis, minus cupiditate. Impedit
+      laborum vero nihil dolor, ullam deserunt hic, veritatis quam perferendis
+      illo perspiciatis et iste corporis est repellat. Itaque, ad numquam ipsa
+      animi consequatur commodi ipsum similique excepturi tempore quae provident
+      fugit ducimus expedita dolore. Deserunt exercitationem nulla natus sit
+      porro praesentium atque nobis iste aspernatur ullam iusto cum tenetur non,
+      repellendus ducimus eaque hic. Qui velit libero nesciunt nulla molestias,
+      ea perspiciatis vero quia optio architecto dolor exercitationem totam
+      ratione tempora cumque deserunt eum, quasi facere sequi dolore cum unde.
+      Culpa fugit laboriosam consequatur fugiat nemo nam dolorem eius.
+      Perspiciatis eveniet labore quo sit officia at minus rem quas pariatur
+      magnam ratione sapiente a sunt, excepturi distinctio! Mollitia, animi,
+      placeat fugit atque, deserunt commodi voluptates aspernatur reiciendis
+      quod quis quos soluta amet quo? Distinctio corporis repellat quis quidem
+      rem ab ut tempora nam, quas quisquam dignissimos quibusdam iure esse
+      architecto non omnis. Nemo laborum quo tempore veniam itaque ducimus autem
+      quia? Laborum sed quibusdam reiciendis voluptate iste voluptatum soluta
+      commodi magnam dolore nisi natus aliquid laboriosam dicta, numquam illo
+      tempora error consectetur exercitationem ipsam beatae cum cumque. Iusto,
+      fugit explicabo, vel repellendus in a iste debitis mollitia porro alias,
+      assumenda nihil eum quisquam? Laborum, eos. Ut esse molestiae sit impedit
+      alias molestias quo necessitatibus officia dicta ullam repellat, deserunt
+      tempora facilis saepe obcaecati eum quidem eligendi! Dicta dolores magnam
+      consequatur, alias labore repudiandae, suscipit ut ducimus saepe
+      recusandae impedit neque asperiores, doloremque cupiditate! Quisquam quis
+      pariatur nostrum eius exercitationem necessitatibus sit accusantium nam a
+      doloremque incidunt, harum eum itaque, sequi, amet quaerat accusamus.
+      Repudiandae corrupti voluptatibus ducimus labore velit unde doloremque
+      delectus perferendis tenetur temporibus eius iusto obcaecati odit debitis,
+      odio autem nesciunt quia eaque sapiente. Ipsum omnis optio consequuntur
+      dolores repellat reiciendis in corporis minus, quibusdam excepturi.
+      Cupiditate numquam nobis tenetur dolores repellat inventore, repellendus
+      dolor magni ex iure esse excepturi optio doloremque iusto placeat fugiat
+      perspiciatis doloribus in ab? Autem quas, iure inventore possimus sed
+      cumque neque, aliquid facere, beatae quis assumenda ipsa? Mollitia, alias.
+      Iste repudiandae architecto dignissimos! Quis illum recusandae beatae
+      alias ipsum explicabo blanditiis magni voluptate dolores, itaque sapiente?
+      Voluptas ducimus a nesciunt magnam nemo, necessitatibus non itaque quia
+      assumenda reprehenderit eveniet accusamus corporis maxime officia,
+      perferendis excepturi deleniti nobis rem saepe odio veritatis. Quos
+      obcaecati molestiae exercitationem odit voluptates excepturi ratione
+      cumque? Doloremque illum sequi voluptates, quas corrupti omnis? Excepturi,
+      veritatis esse enim perferendis atque officiis eligendi vero aliquid,
+      incidunt autem asperiores. Doloribus ea quaerat a dignissimos ipsam
+      nesciunt repellat nisi! Excepturi dicta consectetur aut, consequuntur
+      minima iusto molestiae ipsa quasi ducimus, inventore ea numquam unde
+      culpa, sequi incidunt assumenda qui laudantium eos eius laborum quod
+      adipisci est! Earum neque sunt sapiente, soluta placeat laudantium impedit
+      cupiditate aperiam accusantium, ex vel vero maiores quis ipsum odit
+      tempore sit, ullam asperiores distinctio ratione? Facere possimus
+      laudantium officiis aut unde dicta odio tempore eos ipsa, sint corporis ex
+      rerum ipsum iusto perspiciatis expedita suscipit ab, cumque animi veniam
+      praesentium id? Est impedit officiis facere consectetur fugiat sapiente
+      nulla delectus, placeat accusantium, saepe sint corporis! Aut, suscipit
+      perferendis. Dicta beatae perferendis, molestiae, tempora sed rerum porro
+      dolor suscipit quisquam accusantium, voluptas sapiente temporibus fugiat
+      obcaecati accusamus quidem facilis vitae cupiditate id recusandae!
+      Deleniti autem, velit voluptatibus iusto fuga quisquam enim quaerat veniam
+      quos, consectetur ex mollitia iure accusamus in animi assumenda, adipisci
+      aut ipsam beatae aspernatur a! Corporis libero nulla facere quasi ab hic
+      facilis aliquam vero modi, eos omnis voluptatum, esse itaque!
+      Reprehenderit fuga, et voluptates, suscipit repellendus officia eum nulla
+      obcaecati itaque adipisci nam minus, est quae tempora perspiciatis nemo
+      magni aspernatur dolore laborum quia quos? Culpa laudantium aut cupiditate
+      quia ab a, delectus labore, dolorem amet et, aliquam quo minus provident!
+      Porro animi reprehenderit magni quaerat eius voluptates labore,
+      repudiandae amet adipisci sint quidem consectetur vero optio sed in, minus
+      repellat, earum suscipit debitis eum iure velit libero? Ducimus ut,
+      tenetur atque earum voluptatum ad ullam sunt ipsum? A omnis, amet quo quia
+      ut reprehenderit. Voluptatibus eaque assumenda repellendus laboriosam
+      velit dicta sapiente optio impedit ipsam esse magnam laborum molestias
+      similique cumque aliquid, officia id obcaecati harum aperiam aliquam
+      provident, vel ab. Earum id quaerat incidunt, voluptatibus deserunt sunt
+      esse pariatur cum accusantium quae molestias unde inventore consequuntur?
+    </div>
+  );
+};
+export default Home;
---
+++ components/Footer.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="grid grid-cols-1 divide-y divide-divider-gray">
+        <div className="flex justify-items-stretch h-auto">
+          <div className="flex items-center basis-1/2 p-16">
+            <img className="w-32" src="/assets/images/logo.svg" alt="logo" />
+          </div>
+          <div className="basis-1/2">
+            <div className="flex flex-col justify-center px-24 py-16">
+              <p className="font-bold">開發部</p>
+              <p className="font-normal">
+                邵筱庭 / 王柏涵 / 邱奕庭 / 林柏宇 / 侯博軒 / 王皓平 / 曾家祐 /
+                徐瑞澤
+              </p>
+              <p className="font-bold">開發部</p>
+              <p className="font-normal">
+                邵筱庭 / 王柏涵 / 邱奕庭 / 林柏宇 / 侯博軒 / 王皓平 / 曾家祐 /
+                徐瑞澤
+              </p>
+              <p className="font-bold">開發部</p>
+              <p className="font-normal">
+                邵筱庭 / 王柏涵 / 邱奕庭 / 林柏宇 / 侯博軒 / 王皓平 / 曾家祐 /
+                徐瑞澤
+              </p>
+              <p className="font-bold">開發部</p>
+              <p className="font-normal">
+                邵筱庭 / 王柏涵 / 邱奕庭 / 林柏宇 / 侯博軒 / 王皓平 / 曾家祐 /
+                徐瑞澤
+              </p>
+              <p className="font-bold">開發部</p>
+              <p className="font-normal">
+                邵筱庭 / 王柏涵 / 邱奕庭 / 林柏宇 / 侯博軒 / 王皓平 / 曾家祐 /
+                徐瑞澤
+              </p>
+              <p className="font-bold">開發部</p>
+              <p className="font-normal">
+                邵筱庭 / 王柏涵 / 邱奕庭 / 林柏宇 / 侯博軒 / 王皓平 / 曾家祐 /
+                徐瑞澤
+              </p>
+              <p className="font-bold">開發部</p>
+              <p className="font-normal">
+                邵筱庭 / 王柏涵 / 邱奕庭 / 林柏宇 / 侯博軒 / 王皓平 / 曾家祐 /
+                徐瑞澤
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex px-20 h-40">
+          <div className="basis-1/2 flex flex-col justify-center gap-y-2">
+            <div className="flex gap-x-7">
+              <div>
+                <a href="https://www.facebook.com/meichu.hackathon">
+                  <img
+                    className="w-6"
+                    src="/assets/icons/fb_icon.svg"
+                    alt="facebook"
+                  />
+                </a>
+              </div>
+              <div>
+                <a href="https://www.instagram.com/mc_hackathon/">
+                  <img
+                    className="w-6"
+                    src="/assets/icons/ig_icon.svg"
+                    alt="instagram"
+                  />
+                </a>
+              </div>
+              <div>
+                <a href="https://youtube.com/shorts/iCx_oq5jtQ4?feature=share">
+                  <img
+                    className="w-6"
+                    src="/assets/icons/mail_icon.svg"
+                    alt="mail"
+                  />
+                </a>
+              </div>
+            </div>
+            <div>
+              <p>Copyright © 2023 MeiChu Hackathon</p>
+            </div>
+          </div>
+          <div className="flex basis-1/2 justify-end items-center">
+            <div className="">
+              <a href="https://www.youtube.com/watch?v=ebxn6TlM6EY&ab_channel=%E6%88%91%E6%98%AF%E9%B3%A5%E4%BA%BA">
+                成果存放平台
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
