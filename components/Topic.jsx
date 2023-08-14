83c6f112d10a55ba90e00954ea8e2be2a2173da7
wl00795897 <wl00795897@gmail.com>
2023-08-15 03:30:13 +0800

    Still the same

--- components/Topic.jsx
+++ components/Topic.jsx
@@ -51,7 +51,7 @@ const Topic = () => {
   ];
   return (
     <>
-      <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 p-36 gap-5">
+      <div className="grid grid-cols-2  place-items-center p-[5vw] gap-6">
         {contents.map((contents, index) => {
           return <SubTopic key={index} contents={contents} />;
         })}
@@ -65,21 +65,21 @@ const SubTopic = ({ contents }) => {
 
   return (
     <div
-      className="relative max-w-[21.7rem] max-h-[17.3rem] aspect-[5/4] bg-topic-gray flex flex-col pl-8"
+      className="flex flex-col w-[80vw] max-w-[21.7rem] lg:w-[60vw] aspect-[5/4] bg-topic-gray pl-8"
       onMouseEnter={() => setIsHover(true)}
       onMouseLeave={() => setIsHover(false)}
     >
       <div
-        className={`flex-1 flex flex-col justify-center pt-5 ${
+        className={`basis-1/3 flex flex-col justify-center pt-5 ${
           isHover && "hidden"
         }`}
       >
         <p className="leading-6">{contents.type}</p>
         <p className="text-xl">{contents.company}</p>
       </div>
-      <div className={`flex ${isHover && "hidden"}`}>
+      <div className={`basis-1/3 ${isHover && "hidden"}`}>
         <Image
-          className="w-24 py-5"
+          className="w-24"
           src={TSMC}
           alt="tsmc"
           width={100}
@@ -87,13 +87,11 @@ const SubTopic = ({ contents }) => {
         />
       </div>
       <div
-        className={`absolute inset-0 p-8 ${
-          isHover ? "opacity-100" : "opacity-0"
-        }`}
+        className={`basis-2/3 pt-[20%] pr-8 ${isHover ? "block" : "hidden"}`}
       >
         <p className="text-base font-bold">{contents.topic}</p>
       </div>
-      <div className="flex-1 grid place-items-end pb-6 pr-8">
+      <div className="grid place-content-end basis-1/3 pb-6 pr-8">
         <div className="flex flex-row gap-x-2.5 justify-center">
           <Image
             className="animate-bounce w-6 h-6 place-self-center"
@@ -118,36 +116,9 @@ const SubTopic = ({ contents }) => {
 export default Topic;
 
 /*
-<div
-className="group max-w-[21.7rem] max-h-[17.3rem] aspect-[5/4] bg-topic-gray flex flex-col pl-8"
-onMouseEnter={handleMouseEnter}
-onMouseLeave={handleMouseLeave}
->
-<div className="group-hover:hidden basis-1/3 flex flex-col justify-center">
-  <p className="leading-8">{contents.type}</p>
-  <p className="text-xl">{contents.company}</p>
-</div>
-<Image
-  className="basis-1/3 group-hover:hidden"
-  src={TSMC}
-  alt="tsmc"
-  width={100}
-  height={100}
-/>
-<div className="hidden group-hover:block basis-2/3 pt-16 pr-8">
-  <p className="text-base font-bold">{contents.topic}</p>
-</div>
-<div className="basis-1/3 grid place-items-end pb-6 pr-8">
-  <div className="flex flex-row gap-x-2.5 justify-center">
-    <Image
-      className="animate-bounce w-6 h-6 place-self-center"
-      src={icon_src}
-      alt="more"
-      width={21}
-      height={21}
-    />
-    <p className="text-base opacity-70">{more}</p>
-  </div>
-</div>
-</div>
+      <div className="flex flex-col items-center md:grid md:grid-cols-2 md:content-evenly md:p-[5vw] gap-5">
+        {contents.map((contents, index) => {
+          return <SubTopic key={index} contents={contents} />;
+        })}
+      </div>
 */
