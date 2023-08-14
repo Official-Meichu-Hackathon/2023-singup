0a0d2dcebede9dd809582a42aec31b362de3508e
r1407p <buszter0403@gmail.com>
2023-08-14 08:27:22 +0800

    warning: circle with rwd will have bug

--- components/CountDown.jsx
+++ components/CountDown.jsx
@@ -14,19 +14,21 @@ const CountDown = () =>{
         <div className = "w-full h-auto bg-gray-100 flex justify-between">
             <div className="w-full aspect-[1/1]"> </div>
             <div className="w-full aspect-[1/1] bg-gray-300"> temp </div>
-            <div className="w-full aspect-[1/1]"> <Circle/> </div>
+            <div className="w-full aspect-[1/1]"> <Circle /> </div>
             <div className="w-full aspect-[1/1] bg-gray-300"> temp </div>
         </div>
     );
 };
 
 function Circle (){
+    const PI = 3.141592650897935;
+    const a = 120
     return (
     <>  
             {/* <p className = "bg-gray-400">Countdown Timer</p> */}
-            <div className ="h-full w-full">
-                <svg className="w-full h-full">
-                    <circle className=""
+            <div className ="h-full w-full relative">
+                <svg className="w-full h-full top-0">
+                    <circle className="count"
                         cx='50%'
                         cy='50%'
                         r="47%" 
@@ -35,19 +37,20 @@ function Circle (){
                         stroke-width="3%">
                     </circle>
                 </svg>
-                {/* <svg className="countdown_circle">
-                    <circle 
+                <svg className="w-full h-full  top-0 left-0 static ">
+                    <circle className=""
                         stroke-dasharray="628.3185307179587" 
-                        stroke-dashoffset={a} 
-                        r="100" 
-                        cx="100" 
-                        cy="100" 
+                        stroke-dashoffset="323"
+                        cx='50%'
+                        cy='50%'
+                        r="47%" 
                         fill="none" 
+                        stroke-width="4.5%"
                         stroke-linecap="round" 
                         stroke="lightgreen" 
-                        stroke-width="12">                        
+                        >                        
                     </circle>
-                </svg> */}
+                </svg>
             </div>
         </>
     );
