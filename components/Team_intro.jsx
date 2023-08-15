1e5cb1837f9d564ae85c8f208ba870932395ec1a
wl00795897 <wl00795897@gmail.com>
2023-08-16 00:17:20 +0800

    RWD finished

--- components/Team_intro.jsx
+++ components/Team_intro.jsx
@@ -9,7 +9,7 @@ const Team_intro = () => {
   };
 
   return (
-    <div className="container mx-auto flex justify-center gap-x-[4.5rem] px-12">
+    <div className="container mx-auto flex flex-col md:flex-row items-center md:justify-center gap-[3%] lg:gap-[5%] px-[8%]">
       <Team_each
         imageSrc="/assets/images/創客.jpg"
         description="Image Description"
@@ -33,7 +33,7 @@ const Team_each = ({ imageSrc, description }) => {
     <div
       className={`${
         isHovered ? "bg-[#6A747B]" : "bg-[#F5F5F6]"
-      } duration-500 transition-colors relative overflow-hidden w-[32rem] aspect-square border border-gray-300 rounded shadow-md`}
+      } duration-500 transition-colors relative overflow-hidden w-[75vw] sm:w-[65vw] md:w-[32rem] aspect-[20/21] border border-gray-300 rounded shadow-md`}
       onMouseEnter={handleHover}
       onMouseLeave={handleHover}
     >
@@ -46,8 +46,8 @@ const Team_each = ({ imageSrc, description }) => {
           }`}
         />
         {isHovered && (
-          <div className="absolute w-full h-full top-0 pt-10 px-5 bg-gray-200  bg-opacity-80 transition-opacity duration-500">
-            <p>
+          <div className="text-[2.5vw] md:text-[1.25vw] absolute w-full h-full top-0 pt-[7%] px-[7%] bg-gray-200  bg-opacity-80 transition-opacity duration-500">
+            <p className="leading-normal">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
               blanditiis voluptates iste nemo quod deleniti aliquid saepe, quam
               reiciendis ad numquam! Commodi ratione sint eum sequi blanditiis,
@@ -59,10 +59,25 @@ const Team_each = ({ imageSrc, description }) => {
           </div>
         )}
       </div>
-      <div className="w-1/2 pl-[1.2rem] text-[#6A747B] mt-[0.5rem] text-[2.5rem] bg-white ">
-        黑客組
+      <div className="w-1/2 pl-[5%] text-[#6A747B] mt-[0.75rem] bg-white ">
+        <div className="text-[5vw] md:text-[2.75vw]">黑客組</div>
       </div>
-      <div className="pl-[1.2rem] text-[#6A747B] text-base">黑客組</div>
+      <div
+        className={`${
+          isHovered ? "text-white" : "text-[#6A747B]"
+        } absolute flex bottom-[3%] gap-x-1 sm:gap-x-2 pl-[5%] text-base`}
+      >
+        <img
+          src={
+            isHovered
+              ? "/assets/icons/topic_arrow_w.svg"
+              : "/assets/icons/topic_arrow.svg"
+          }
+          alt=""
+          className="w-[17%] md:w-auto aspect-square"
+        />
+        <p className="text-[2.5vw] md:text-[1.25vw]">工作坊細節</p>
+      </div>
     </div>
   );
 };
