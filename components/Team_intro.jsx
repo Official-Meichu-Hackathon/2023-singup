00e337c4fb024a96230ebab451aa901af75364e7
r1407p <buszter0403@gmail.com>
2023-08-15 11:37:06 +0800

    feat: turn pic to text

--- components/Team_intro.jsx
+++ components/Team_intro.jsx
@@ -14,20 +14,26 @@ const Team_intro = ({ imageSrc, description }) => {
       onMouseEnter={handleHover}
       onMouseLeave={handleHover}
     >
-    <div>
-      <img
-        src={imageSrc}
-        alt="Transforming"
-        className={`w-auto h-auto object-cover transition-opacity duration-500 ${
-          isHovered ? 'opacity-0' : 'opacity-100'
-        }`}
-      />
-      </div>
-      {isHovered && (
-        <div className="absolute inset-0 flex items-center justify-center bg-gray-300 bg-opacity-80 text-white text-center transition-opacity duration-500">
-          {description}
+        <div className = 'w-auto h-auto relative'>
+            <img
+                src={imageSrc}
+                alt="Transforming"
+                className={`w-auto h-auto object-cover transition-opacity duration-500 ${
+                isHovered ? 'opacity-0' : 'opacity-100'
+                }`}
+            />
+            {isHovered && (
+            <div className = 'absolute w-full h-full  top-0  bg-gray-200  bg-opacity-80 transition-opacity duration-500'>
+                <h3>黑客組</h3>
+            </div>
+        )}
+            
         </div>
-      )}
+        {/* {isHovered && (
+            <div className="absolute inset-0 flex items-center justify-center bg-gray-300 bg-opacity-80 text-white text-center transition-opacity duration-500">
+            {description}
+            </div>
+        )} */}
     </div>
   );
 };
