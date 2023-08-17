a36aeca772cc13e950ecadc1c22b0ca54000dd61
wl00795897 <wl00795897@gmail.com>
2023-08-17 20:12:45 +0800

    RWD Finished

--- components/Sponsor.jsx
+++ components/Sponsor.jsx
@@ -5,39 +5,133 @@ const Sponsor = () => {
     {
       imgSrc: "/assets/images/ETtoday_logo.png",
       name: "ETToday",
-      size: "h-full",
+      size: "max-h-full",
     },
-    { imgSrc: "/assets/images/NXP_logo.jpeg", name: "NXP", size: "h-full" },
+    {
+      imgSrc: "/assets/images/NXP_logo.jpeg",
+      name: "NXP",
+      size: "max-h-[90%]",
+    },
     {
       imgSrc: "/assets/images/Google_logo.jpeg",
       name: "Google",
-      size: "h-3/5",
+      size: "max-h-[60%] px-3",
     },
-    { imgSrc: "/assets/images/NXP_logo.jpeg", name: "NXP", size: "h-full" },
+    {
+      imgSrc: "/assets/images/line_logo.png",
+      name: "LINE",
+      size: "max-h-full",
+    },
+  ];
+  const golds = [
+    {
+      imgSrc: "/assets/images/logi_logo.png",
+      name: "Logitech",
+      size: "max-h-[40%] px-5",
+    },
+  ];
+  const silvers = [
+    {
+      imgSrc: "/assets/images/104_logo.png",
+      name: "104",
+      size: "max-h-full",
+    },
+    {
+      imgSrc: "/assets/images/ctbc_logo.png",
+      name: "中國信託",
+      size: "max-h-full",
+    },
+    {
+      imgSrc: "/assets/images/def_logo.png",
+      name: "原相",
+      size: "max-h-full",
+    },
+  ];
+  const bronzes = [
+    {
+      imgSrc: "/assets/images/jing_logo.png",
+      name: "錦明實業",
+      size: "max-h-full",
+    },
   ];
   return (
     <>
       <Sponsor_title title="合作企業" />
-      <div className="flex w-full gap-5">
+      <div className="flex w-full flex-wrap gap-5 ">
         {cooperations.map((cooperation, index) => {
           return <Sponsor_each key={index} cooperation={cooperation} />;
         })}
       </div>
       <Sponsor_title title="贊助企業" />
-      <span className="text-xl font-bold text-center py-3 tracking-[0.15em]">
+      <span className="text-xl font-bold text-center tracking-[0.15em]">
         Platinum
       </span>
-      <div className="flex w-full gap-5">
+      <div className="flex w-full py-4 flex-wrap justify-center gap-x-5 gap-y-3">
         {cooperations.map((cooperation, index) => {
           return <Sponsor_each key={index} cooperation={cooperation} />;
         })}
       </div>
-      <Sponsor_title title="特殊贊助" />
+      <div className="flex w-full flex-wrap justify-evenly gap-x-5 gap-y-3">
+        <div className="flex flex-col items-center">
+          <span className="text-xl font-bold text-center tracking-[0.15em]">
+            Gold
+          </span>
+          <div className="flex w-full flex-wrap py-4 gap-5">
+            {golds.map((gold, index) => {
+              return <Sponsor_each key={index} cooperation={gold} />;
+            })}
+          </div>
+        </div>
+        <div className="flex flex-col items-center">
+          <span className="text-xl font-bold text-center tracking-[0.15em]">
+            Silver
+          </span>
+          <div className="flex w-full flex-wrap py-4 gap-5">
+            {silvers.map((silver, index) => {
+              return <Sponsor_each key={index} cooperation={silver} />;
+            })}
+          </div>
+        </div>
+        <div className="flex flex-col items-cente">
+          <span className="text-xl font-bold text-center tracking-[0.15em]">
+            Bronze
+          </span>
+          <div className="flex w-full flex-wrap py-4 gap-5">
+            {bronzes.map((bronze, index) => {
+              return <Sponsor_each key={index} cooperation={bronze} />;
+            })}
+          </div>
+        </div>
+      </div>
 
-      <Sponsor_title title="這是廣告"/>
-      <a href="https://www.nxp.com" target="_blank" className="w-[80%] md:w-[70%] lg:w-[60%] my-[1%] block">
-      <object data="/assets/images/sponsor_nxp.svg" type="image/svg+xml" className="w-full rounded-[1.25rem] pointer-events-none">
-        </object>
+      <Sponsor_title title="特殊贊助" />
+      <span className="text-xl font-bold text-center tracking-[0.15em]">
+        Bronze
+      </span>
+      <div className="flex w-full py-4 flex-wrap justify-center gap-x-5 gap-y-3">
+        {cooperations.map((cooperation, index) => {
+          return <Sponsor_each key={index} cooperation={cooperation} />;
+        })}
+      </div>
+      <span className="text-xl font-bold text-center tracking-[0.15em]">
+        Freshmen
+      </span>
+      <div className="flex w-full py-4 flex-wrap justify-center gap-x-5 gap-y-3">
+        {cooperations.map((cooperation, index) => {
+          return <Sponsor_each key={index} cooperation={cooperation} />;
+        })}
+      </div>
+      <Sponsor_title title="這是廣告" />
+      <a
+        href="https://www.nxp.com"
+        target="_blank"
+        className="w-[80%] md:w-[70%] lg:w-[60%] my-[1%] block"
+      >
+        <object
+          data="/assets/images/sponsor_nxp.svg"
+          type="image/svg+xml"
+          className="w-full rounded-[1.25rem] pointer-events-none"
+        ></object>
       </a>
     </>
   );
@@ -48,9 +142,9 @@ const Sponsor_title = ({ title }) => {
     <>
       <div class="relative w-full flex py-5 items-center">
         <div class="grow w-full border-t border-black"></div>
-        <span class="sponsor_title flex-shrink w-1/2 px-12 py-0.5">
+        <h3 class="sponsor_title whitespace-nowrap flex-shrink w-[1/2] px-[10%] md:px-[5%] py-1.5">
           {title}
-        </span>
+        </h3>
         <div class="grow w-full border-t border-black"></div>
       </div>
     </>
@@ -61,7 +155,7 @@ const Sponsor_each = ({ cooperation }) => {
   return (
     <>
       <div className="grow flex flex-col items-center">
-        <div className="flex items-center h-[6.75rem] w-auto bg-white">
+        <div className="flex items-center h-[6.75rem] bg-white">
           <img className={cooperation.size} src={cooperation.imgSrc} alt="" />
         </div>
         <span className="text-base">{cooperation.name}</span>
