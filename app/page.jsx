4ce15e9f6dfa4cbf687fd5816ca4b981e986d9aa
r1407p <buszter0403@gmail.com>
2023-08-25 01:58:50 +0800

    fix: global.css redundant

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,8 @@ import News from "@components/News";
 import Timetable from "@components/Timetable";
 import Sign_in from "@components/Sign-in"
 import Activity from "@components/Activity";
+import Team_intro from "@components/Team_intro";
+import Award from "@components/Award";
 const Home = () => {
   return (
     <>
