7acad1e41ba4e4a8e8a1658739032405f4a24c14
clyee <happying910807@gmail.com>
2023-08-29 21:50:51 +0800

    wave animation

--- components/Page_title.jsx
+++ components/Page_title.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import WaveAnimation from "@styles/WaveAnimation";
 
 const Page_title = () => {
     return(
@@ -7,7 +8,10 @@ const Page_title = () => {
             <h4 className="absolute title_bg ">
               2023
             </h4>
-            <img src = '/assets/images/bg-spin.svg' alt = "bg" className = 'w-full h-auto pt-[10%]'/>
+            <div className="absolute">
+              <WaveAnimation/>
+            </div>
+            {/* <img src = '/assets/images/bg-spin.svg' alt = "bg" className = 'w-full h-auto pt-[10%]'/> */}
             <h1 className = 'absolute m-auto h-full top-[9%] left-[47%] text-[6vw] w-0 leading-normal '> 梅竹黑客松</h1>
             <h4 className = 'absolute right-[5%] text-[4vw] bottom-[17%] hack_font'>Oct. 21-22</h4>
             <h6 className = 'absolute right-[7%] text-[2vw] bottom-[13%] hack_font text-[#6A747B]'>11th MeiChu Hackathon</h6>
