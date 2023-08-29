54f0a3c62763a42d92b199aa84d600fc96c883c9
r1407p <buszter0403@gmail.com>
2023-08-29 19:11:34 +0800

    fix: hsinchu

--- components/Page_title.jsx
+++ components/Page_title.jsx
@@ -8,7 +8,17 @@ const Page_title = () => {
               2023
             </h4>
             <img src = '/assets/images/bg-spin.svg' alt = "bg" className = 'w-full h-auto pt-[10%]'/>
-            <h1 className = 'absolute m-auto h-full top-[9%] left-[47%] text-[6vw] w-0 leading-normal '> 梅竹黑客松</h1>
+            <div className = 'absolute m-auto h-full top-[9%] left-[47%] text-[4vw] w-[4vw] leading-snug flex flex-col'>
+              <h1 className  ='text-[4vw] w-[4vw] leading-snug'>新</h1>
+              <h1 className  ='text-[4vw] w-[4vw] leading-snug'>竹</h1>
+              <h1 className  ='text-[4vw] w-[4vw] leading-snug mx-auto'>X</h1>
+              <h1 className  ='text-[4vw] w-[4vw] leading-snug'>梅</h1>
+              <h1 className  ='text-[4vw] w-[4vw] leading-snug'>竹</h1>
+              <h1 className  ='text-[4vw] w-[4vw] leading-snug'>黑</h1>
+              <h1 className  ='text-[4vw] w-[4vw] leading-snug'>客</h1>
+              <h1 className  ='text-[4vw] w-[4vw] leading-snug'>松</h1>
+              
+              </div>
             <h4 className = 'absolute right-[5%] text-[4vw] bottom-[17%] hack_font'>Oct. 21-22</h4>
             <h6 className = 'absolute right-[7%] text-[2vw] bottom-[13%] hack_font text-[#6A747B]'>11th MeiChu Hackathon</h6>
             <img src = '/assets/images/ball.svg' alt = "bg" className = 'w-[8%] right-[15%] top-[8%] h-auto absolute'/>
@@ -20,7 +30,16 @@ const Page_title = () => {
               2023
             </h4>
             <img src = '/assets/images/bg-straight.svg' alt = "bg" className = 'w-full h-auto pt-[10%]'/>
-            <h1 className = 'absolute m-auto h-full top-[15%] left-[47%] text-[12vw] w-0 leading-normal '> 梅竹黑客松</h1>
+            <div className = 'absolute m-auto h-full top-[8%] left-[47%] text-[12vw] w-[12vw] leading-normal flex flex-col '>
+              <h1 className  ='text-[12vw] w-[12vw] leading-snug'>新</h1>
+              <h1 className  ='text-[12vw] w-[12vw] leading-snug'>竹</h1>
+              <h1 className  ='text-[12vw] w-[12vw] leading-snug mx-auto'>X</h1>
+              <h1 className  ='text-[12vw] w-[12vw] leading-snug'>梅</h1>
+              <h1 className  ='text-[12vw] w-[12vw] leading-snug'>竹</h1>
+              <h1 className  ='text-[12vw] w-[12vw] leading-snug'>黑</h1>
+              <h1 className  ='text-[12vw] w-[12vw] leading-snug'>客</h1>
+              <h1 className  ='text-[12vw] w-[12vw] leading-snug'>松</h1>
+              </div>
             <h4 className = 'absolute right-[5%] text-[6vw] bottom-[21%] hack_font'>Oct. 21-22</h4>
             <h6 className = 'absolute right-[7%] text-[5vw] bottom-[15%] hack_font text-[#6A747B]'>11th MeiChu Hackathon</h6>
         </div>
