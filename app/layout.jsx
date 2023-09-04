f64ab19c604fcfbc585e3e5bda318a8ddec08f9c
r1407p <buszter0403@gmail.com>
2023-09-05 00:40:30 +0800

    feat: add icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,19 +8,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const metadata = {
   title: "2023 meichu hackathon",
   image: "/assets/images/logo.svg",
+  icon: "/assets/icons/favicon.ico"
 };
 
 const RootLayout = ({ children }) => (
   <html lang="en">
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css?family=Noto+Serif+TC"
-    ></link>
-    <link
-      rel="icon"
-      href="/assets/icons/favicon.ico"
-      type="image/x-icon"
-    ></link>
+    <head>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Noto+Serif+TC"
+      ></link>
+      <link
+        rel="icon"
+        href="/assets/icons/favicon.ico"
+        type="image/x-icon"
+      ></link>
+    </head>
     <body className="bg-[#F5F5F6] top-0 absolute ">
       <NavBar />
       <main className="py-14">{children}</main>
