13bf119568dbced6bf30e4c2282a24be3ffab7ef
r1407p <buszter0403@gmail.com>
2023-09-19 19:13:15 +0800

    feat: metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,14 +6,19 @@ import Footer from "@components/Footer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const metadata = {
-  title: "2023 meichu hackathon",
+  title: "2023 新竹X梅竹黑客松",
   image: "/assets/images/logo.svg",
-  icon: "/assets/icons/favicon.ico"
+  icon: "/assets/icons/favicon.ico",
+  url: "https://2023.meichuhackathon.org/",
+  description: "「2023 新竹 X 梅竹黑客松」 延續前一屆種下的創意種子，迎接新一年的盛會期待百花綻放，在近年人工智慧與科技蓬勃發展的洪流下，我們邀請到六間國際級企業與新竹市政府，剖析世界所期待得到解答的問題，並提供相關資源與指導，引領所有參賽者成為關鍵的最後一塊拼圖。",
+
 };
 
 const RootLayout = ({ children }) => (
   <html lang="en">
     <head>
+    <meta name="description" content={metadata.description}/>
+      <title>{metadata.title}</title>
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Noto+Serif+TC"
